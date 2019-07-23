49534eefcc8dca12be01179bc29f6be98c3dcc0e
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-23 19:55:13 +0300

    Pirma užduotis console aplikacija

---
+++ _20190723Pirmas/_20190723Pirmas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190723Pirmas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("To Do sąrašas: ");
+            Console.WriteLine("1. Išnešti šiūkšles");
+            Console.WriteLine("2. Nupirkti pieno");
+            Console.WriteLine("3. Pavedžioti šunį");
+        }
+    }
+}
