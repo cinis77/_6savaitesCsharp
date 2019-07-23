3a2a4774f73ebbf97c28d8daed8c2e25bf6c4e9b
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-23 20:38:59 +0300

    Pirmas pavyzdys su informacijos ivedimu

---
+++ _20190723_SkaitymasIsConsole/_20190723_SkaitymasIsConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190723_SkaitymasIsConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite tekstą: ");
+            string tekstasIšVartotojo = Console.ReadLine();
+            Console.WriteLine("Jūsų įvestas tekstas yra: ");
+            Console.WriteLine(tekstasIšVartotojo);
+            Console.ReadKey();
+        }
+    }
+}
