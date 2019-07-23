94735173be881b38dfafcb3c5fab21ac1c2e34d3
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-23 21:17:19 +0300

    Uzduotis aritmetiniai veiksmai

---
+++ _20190723_AritmetiniaiVeiksmai/_20190723_AritmetiniaiVeiksmai/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190723_AritmetiniaiVeiksmai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu:");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu:");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            int suma = sk + sk1;
+            int skirtumas = sk - sk1;
+            int sandauga = sk * sk1;
+            double dalyba = (double)sk / sk1;
+            Console.WriteLine("{0} + {1} = {2}", sk, sk1, suma);
+            Console.WriteLine("{0} - {1} = {2}", sk, sk1, skirtumas);
+            Console.WriteLine("{0} * {1} = {2}", sk, sk1, sandauga);
+            Console.WriteLine("{0} / {1} = {2}", sk, sk1, dalyba);
+        }
+    }
+}
