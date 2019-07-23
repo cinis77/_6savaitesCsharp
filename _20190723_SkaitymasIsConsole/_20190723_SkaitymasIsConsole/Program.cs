7f8a9d0825c81eabd5b75201bbe48f0173f8ad5f
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-23 20:52:06 +0300

    Antra uzduotis su konsole aplikacijos ivedimu informacijos

--- _20190723_SkaitymasIsConsole/_20190723_SkaitymasIsConsole/Program.cs
+++ _20190723_SkaitymasIsConsole/_20190723_SkaitymasIsConsole/Program.cs
@@ -14,6 +14,20 @@ namespace _20190723_SkaitymasIsConsole
             string tekstasIšVartotojo = Console.ReadLine();
             Console.WriteLine("Jūsų įvestas tekstas yra: ");
             Console.WriteLine(tekstasIšVartotojo);
+
+            Console.WriteLine("Iveskite savo varda: ");
+            string Vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite pirmos megiamiausios splavos raide");
+            char raide = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine("\nIveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Jusu vardas yra: {0}, megiamiausios"
+                + " spalvos pirma raide {1} ivesti atsitiktiniai"
+                + " skaiciai yra: {2}, {3}", Vardas, raide, sk, sk1);
+
             Console.ReadKey();
         }
     }
