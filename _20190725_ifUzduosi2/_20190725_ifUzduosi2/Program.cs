cfb49c1dc9fd4424d75b07de164356f8849ac3ed
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-25 18:59:34 +0300

    if pasikartojimo uzdaviniai

---
+++ _20190725_ifUzduosi2/_20190725_ifUzduosi2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190725_ifUzduosi2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            if ((skaicius % 2 == 0 && skaicius % 3 == 0) || skaicius % 5 == 0)
+            {
+                Console.WriteLine("Skaicius yra 2 ir 3 kartotinis arba 5 kartotinis");
+            }
+        }
+    }
+}
---
+++ _20190725_ifuzduotis/_20190725_ifuzduotis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190725_ifuzduotis
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a < 100)
+            {
+                Console.WriteLine("Pirmos dalies ispildymas");
+            }
+            else if (b > 0 && b > a)
+            {
+                Console.WriteLine("Antros dalies ispildymas");
+            }
+            else if (a > b && a > c || a > 0)
+            {
+                Console.WriteLine("Trecios dalies ispildymas");
+            }
+            else if (c >= 5 && c <= 10 || c > a || c > b)
+            {
+                Console.WriteLine("Ketvirtos dalies ispildymas");
+            }
+        }
+    }
+}
