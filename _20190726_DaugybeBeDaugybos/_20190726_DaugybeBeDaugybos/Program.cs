2270ca60a908bd3b0d53803259d1c504227c42b8
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-26 19:39:24 +0300

    UZdaviniai klases darbai

---
+++ _20190726_DaugybeBeDaugybos/_20190726_DaugybeBeDaugybos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190726_DaugybeBeDaugybos
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            int suma = 0;
+            for (int i = 0; i < sk1; i++)
+            {
+                suma += sk; // suma = suma + sk
+            }
+            Console.WriteLine("Atsakymas yra: " + suma);
+        }
+    }
+}
---
+++ _20190726_forLaiptai/_20190726_forLaiptai/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190726_forLaiptai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int row = 1; row <= 6; row++)
+            {
+                for (int tarpai = 0; tarpai < 6 - row; tarpai++)
+                {
+                    Console.Write(" ");
+                }
+                for (int hashTag = 0; hashTag < row; hashTag++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
