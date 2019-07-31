34fbc8df64f023e6958546acfe5a178e7a9e9a85
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-07-31 19:21:16 +0300

    Prezidento rinkimai

---
+++ _20190731_masyvaiSuskaiciavimasMasyvoIlgio/_20190731_masyvaiSuskaiciavimasMasyvoIlgio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190731_masyvaiSuskaiciavimasMasyvoIlgio
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            //------------------------------
+            int masyvoIlgis = 0;
+            int skaicius = 1;
+            //------------------------------
+            while (skaicius < sk)
+            {
+                masyvoIlgis++;
+                Console.WriteLine(skaicius);
+                skaicius *= (2); // Math.Pow(2,masyvoIlgis)
+            }
+            Console.WriteLine(masyvoIlgis);
+            int[] masyvas = new int[masyvoIlgis];
+            skaicius = 1;
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                masyvas[i] = skaicius;
+                skaicius *= 2;
+            }
+
+            foreach (var item in masyvas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ _20190731_prezidentoRinkimai/_20190731_prezidentoRinkimai/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190731_prezidentoRinkimai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite kandidatus");
+
+            string kandidatai = Console.ReadLine();
+            string[] kandidatuMasyvas = kandidatai.Split(' ');
+            string kandidatas = kandidatuMasyvas[0];
+            char[] raides = kandidatas.ToCharArray();
+            foreach (var item in raides)
+            {
+                Console.WriteLine(item);
+            }
+
+            int max = int.MinValue;
+            int laimetojas = -1;
+
+            for (int i = 0; i < kandidatuMasyvas.Length; i++)
+            {
+                int unikaliosRaides = KiekYraUnikaliuRaidziu(kandidatuMasyvas[i].
+                    ToLower().ToCharArray());
+                if (unikaliosRaides > max)
+                {
+                    max = unikaliosRaides;
+                    laimetojas = i;
+                }
+            }
+
+            Console.WriteLine("Prezidentu tapo: " + kandidatuMasyvas[laimetojas]);
+        }
+
+        private static int KiekYraUnikaliuRaidziu(char[] raides)
+        {
+            int unikaliuRaidziuKiekis = 0;
+            for (int i = 0; i < raides.Length; i++)
+            {
+                bool unikali = true;
+                for (int j = i + 1; j < raides.Length; j++)
+                {
+                    if (raides[i] == raides[j])
+                    {
+                        //Kartojasi raide
+                        unikali = false;
+                        break;
+                    }
+                }
+
+                if (unikali)
+                {
+                    unikaliuRaidziuKiekis++;
+                }
+            }
+
+            return unikaliuRaidziuKiekis;
+        }
+    }
+}
